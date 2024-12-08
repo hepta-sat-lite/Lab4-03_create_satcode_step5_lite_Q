7b2f5a9bf063a3e6fdc057fbac1a3ba227210f90
Charlie.com <csru22005@g.nihon-u.ac.jp>
2024-12-08 10:12:15 +0000

    fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,111 +1,110 @@
 #include "mbed.h"
-#include "HEPTA_EPS.h"
-#include "HEPTA_CDH.h"
-#include "HEPTA_SENSOR.h"
-HEPTA_CDH cdh(PB_5, PB_4, PB_3, PA_8, "sd");
-HEPTA_EPS eps(PA_0,PA_4);
-HEPTA_SENSOR sensor(PA_7,PB_7,PB_6,0xD0);
-DigitalOut condition(PB_1);
+#include "LITE_CDH.h"
+#include "LITE_EPS.h"
+#include "LITE_SENSOR.h"
+LITE_CDH cdh(PB_5, PB_4, PB_3, PA_8, "sd", PA_3);
+LITE_EPS eps(PA_0,PA_4);
+LITE_SENSOR sensor(PA_7,PB_7,PB_6);
+DigitalOut cond[]={PB_1, PA_5};
 RawSerial gs(USBTX,USBRX,9600);
 Timer sattime;
 int rcmd = 0, cmdflag = 0; //command variable
-
-//getting command and command flag
+ 
+//getting command and flag
 void commandget()
 {
-    rcmd = gs.getc();
+    rcmd=gs.getc();
     cmdflag = 1;
 }
-//interrupting process by command receive
+//interrupting 
 void receive(int rcmd, int cmdflag)
 {
     gs.attach(commandget,Serial::RxIrq);
 }
-//initializing
+//initialzing
 void initialize()
 {
     rcmd = 0;
     cmdflag = 0;
-    condition = 0;
+    cond[0] = 0;
 }
 
-int main()
-{
-    gs.printf("From Sat : Nominal Operation\r\n");
-    int flag = 0; //condition flag
-    float batvol, temp; //voltage, temperature 
+int main() {
+    gs.printf("From Sat : Operation Start...\r\n");
+    int flag = 0;     // condition
+    float batvol,temp; //Voltage, Temerature
+    receive(rcmd,cmdflag); //interupting by ground station command
     sattime.start();
-    receive(rcmd,cmdflag); //interrupting
-    eps.turn_on_regulator();//turn on 3.3V conveter
-    sensor.setup();
-    for(int i=0;i<50;i++){
-        //satellite condition led
-        condition = !condition;
-        
-        //senssing HK data
+    for(int i = 0; i < 100; i++) {
+        //Sensing and Transmitting HK data
         eps.vol(&batvol);
-        sensor.temp_sense(&temp);
-        
-        //Transmitting HK data to Ground Station(GS)
-        gs.printf("HEPTASAT::Condition = %d, Time = %f [s], batvol = %2f [V], temp = %2f [deg C]\r\n",flag,sattime.read(),batvol,temp);
-        wait_ms(1000);
-        
-        //Power Saving Mode 
-        if((batvol <= 3.5)  | (temp > 35.0)){
+        temp = 28.5;
+        gs.printf("HEPTASAT::Condition = %d, Time = %f [s], BatVol = %.2f [V],Temp = %.2f [C]\r\n",flag,sattime.read(),batvol,temp);
+        //Condition
+        cond[0] = 1;
+        //Power Saving Mode
+        if(batvol <= 3.5) {
             eps.shut_down_regulator();
             gs.printf("Power saving mode ON\r\n"); 
             flag = 1;
-        } else if((flag == 1) & (batvol > 3.7) & (temp <= 25.0)) {
+        } else if((flag == 1) & (batvol > 3.7)) {
             eps.turn_on_regulator();
-            gs.printf("Power saving mode OFF\r\n");
             flag = 0;
         }
-        
-        if(cmdflag == 1){
-            if(rcmd == 'a'){
-                for(int j=0;j<5;j++){
-                    gs.printf("Hello World!\r\n");
-                    condition = 1;
-                    wait_ms(1000);
-                }
-            }else if(rcmd == 'b') {
+        //Contents of command
+        if (cmdflag == 1) {
+            if (rcmd == 'a') {
+               for(int j=0;j<5;j++){
+                gs.printf("HEPTASAT::Hello World!\r\n");
+                cond[0] = 0;
+                wait_ms(1000);
+                cond[0] = 1;
+               }
+            }else if (rcmd == 'b') {
                 char str[100];
                 mkdir("/sd/mydir", 0777);
-                FILE *fp = fopen("/sd/mydir/satdata.txt","w");
+                FILE *fp = fopen("/sd/mydir/test.txt","w");
                 if(fp == NULL) {
                     error("Could not open file for write\r\n");
                 }
                 for(int i = 0; i < 10; i++) {
                     eps.vol(&batvol);
                     fprintf(fp,"%f\r\n",batvol);
-                    condition = 1;
-                    wait_ms(1000);
                 }
                 fclose(fp);
-                fp = fopen("/sd/mydir/satdata.txt","r");
-                for(int i = 0; i < 10; i++) {
+                fp = fopen("/sd/mydir/test.txt","r");
+                for(int j = 0; j < 10; j++) {
                     fgets(str,100,fp);
                     gs.puts(str);
                 }
                 fclose(fp);
-            }else if(rcmd == 'c'){
+            }else if (rcmd == 'c') {
+                gs.printf("Hepta-Sat Lite Uplink Ok\r\n");
                 float ax,ay,az;
-                for(int i = 0; i < 10; i++) {
+                sensor.set_up();
+                for(int i = 0; i<10; i++) {
                     sensor.sen_acc(&ax,&ay,&az);
-                    gs.printf("%f,%f,%f\r\n",ax,ay,az);
-                    wait_ms(1000); 
+                    gs.printf("acc : %f,%f,%f\r\n",ax,ay,az);
+                    wait_ms(1000);
                 }
-            }else if(rcmd == 'd'){
+            }else if (rcmd == 'd') {
+                gs.printf("Hepta-Sat Lite Uplink Ok\r\n");
                 float gx,gy,gz;
-                for(int i = 0; i < 10; i++) {
+                sensor.set_up();
+                for(int i = 0; i<10; i++) {
                     sensor.sen_gyro(&gx,&gy,&gz);
-                    gs.printf("%f,%f,%f\r\n",gx,gy,gz); 
-                    wait_ms(1000);   
-                }            
+                    gs.printf("gyro: %f,%f,%f\r\n",gx,gy,gz);
+                    wait_ms(1000);
+                }
             }
-            initialize(); //initializing
+            initialize();
         }
+        
+        //Operation Interval
+        wait_ms(1000);
     }
+    cond[0] = 0;
     sattime.stop();
-    gs.printf("From Sat : End of operation\r\n");
+    gs.printf("From Sat : Operation Stop...\r\n");
 }
+           
